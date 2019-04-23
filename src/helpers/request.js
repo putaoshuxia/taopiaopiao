c02c12eadf16e2d6a9b98ca78825493b769cbb7b
wuyongen <32027187+wuyongen@users.noreply.github.com>
2019-04-23 23:01:21 +0800

    Add files via upload

---
+++ src/helpers/request.js
@@ -0,0 +1,24 @@
+export default async function (path, opt) {
+  // const url = 'https://www.easy-mock.com/mock/5a964cffdd997801d1e291fe/movie' + path;
+  const url = 'http://39.105.203.171:8081' + path;
+
+  const options = Object.assign({
+    method: 'GET',
+  }, opt);
+
+  try {
+    const response = await fetch(url, options);
+    const { data, status } = await response.json();
+
+    if (status === 0) {
+      return data;
+    } else {
+      // 错误处理
+    }
+  } catch (e) {
+    // 错误处理
+    // 提示/弹窗
+  }
+
+}
+
